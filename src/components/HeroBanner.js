32c426cfa8f81d98140abc3845496470b6163f22
radubogdan6466 <radubogdan6466@gmail.com>
2024-06-20 16:17:51 +0300

    update

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../styles/HeroBanner.css";
-import bizImage from "../assets/images/biz.webp";
+// import bizImage from "../assets/images/biz.webp";
 const HeroBanner = () => {
   return (
     <div
       className="hero-banner"
-      style={{ backgroundImage: `url(${bizImage})` }}
+      style={{ backgroundImage: "url('https://i.ibb.co/ysknJWN/biz-2.jpg)" }}
     >
       <h1 className="hero-text">Bizpaint</h1>
     </div>
